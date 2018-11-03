2f0e5fbdb2cc7a1d2ccdab267972ac8f1d0afe61
andgitisaac <andgitisaac@gmail.com>
2018-11-03 15:43:28 +0800

    Path Configurations

---
+++ config.py
@@ -0,0 +1,7 @@
+CONTENT_PATH = "/home/andgitisaac/dataset/coco/train2017/"
+STYLE_PATH = "/home/andgitisaac/dataset/kaggle/train/"
+
+PRETRAINED_VGG_PATH = "/home/andgitisaac/FREEStyleGAN/pretrained/vgg_normalised.pth"
+PRETRAINED_ADAIN_PATH = "/home/andgitisaac/FREEStyleGAN/pretrained/adain_decoder.pth"
+PRETRAINED_MATTING_NETWORK_PATH = "/home/andgitisaac/FREEStyleGAN/pretrained/pretrained_mattingNet.pth"
+PRETRAINED_DECODER_PATH = "/home/andgitisaac/FREEStyleGAN/pretrained/pretrained_decoder.pth"
