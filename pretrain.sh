4f042f7a28dc8cd6d07264170a5a2b48b0748b74
andgitisaac <andgitisaac@gmail.com>
2018-11-06 21:52:50 +0800

    comment one line

--- pretrain.sh
+++ pretrain.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python -u pretrain_mattingNet.py
-CUDA_VISIBLE_DEVICES=0 python -u pretrain_decoder.py --save_dir ./checkpoints/pretrained_decoder/ --log_dir 
+#CUDA_VISIBLE_DEVICES=0 python -u pretrain_decoder.py --save_dir ./checkpoints/pretrained_decoder/ --log_dir 
 ./logs/pretrained_decoder/
